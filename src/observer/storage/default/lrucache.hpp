40fe122cb4f5d209cce77db2061659ab82366a16
rehoboan <rehoboam.cc.wang@gmail.com>
2022-03-17 20:06:32 +0800

    add more lru todo

--- src/observer/storage/default/lrucache.hpp
+++ src/observer/storage/default/lrucache.hpp
@@ -24,21 +24,24 @@ namespace cache {
         }
 
         void put(const key_t& key, const value_t& value) {
-          //TODO 实现LRU的put函数
+          // TODO 将页和页地址放到双向链表的head
+          // TODO 如果页已经存在，删除原先双向链表中的页并更新哈希表中页对应的地址
+          // TODO 如果LRU的size超过链最大size，删除双向链表tail的页以及哈希表中的地址
         }
 
         const value_t& get(const key_t& key) {
-          //TODO 实现LRU的get函数
+          // TODO 如果页不存在，throw error
+          // TODO 将双向链表中对应页(key)和页地址更新到head
+          // TODO 返回页的地址
         }
 
         bool exists(const key_t& key) const {
-          //TODO 实现exist函数
-          // key 存在返回true, key不存在返回false
+          // TODO 页(key)存在，返回 true
+          // TODO 页(key)不存在，返回 false
         }
 
         size_t size() const {
-          //TODO 实现size函数
-          // 返回LRU cache size
+          // TODO 返回LRU cache size
         }
 
     private:
