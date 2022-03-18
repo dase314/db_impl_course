5bd39e4767787ac49d59a31475bd910488cf1c20
Aurora <37304578+rehoboan@users.noreply.github.com>
2022-03-18 14:32:12 +0800

    Delete cmake-build-debug directory

--- cmake-build-debug/unitest/log_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/rehoboamw/Lesson/db_impl_course/cmake-build-debug/unitest/log_test[1]_tests.cmake")
-  include("/Users/rehoboamw/Lesson/db_impl_course/cmake-build-debug/unitest/log_test[1]_tests.cmake")
-else()
-  add_test(log_test_NOT_BUILT log_test_NOT_BUILT)
-endif()
--- cmake-build-debug/unitest/rc_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/rehoboamw/Lesson/db_impl_course/cmake-build-debug/unitest/rc_test[1]_tests.cmake")
-  include("/Users/rehoboamw/Lesson/db_impl_course/cmake-build-debug/unitest/rc_test[1]_tests.cmake")
-else()
-  add_test(rc_test_NOT_BUILT rc_test_NOT_BUILT)
-endif()
